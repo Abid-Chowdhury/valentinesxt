ce84cf1dd71e800ca26967db958c947842d86acc
Abid-Chowdhury <mr.abid2004@gmail.com>
2025-12-22 00:23:36 -0500

    Added text to home page Also added a button to take user to "Pictures" page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,21 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Home() {
+  const navigate = useNavigate();
+
   return (
     <div className="page">
-      <h1>Home</h1>
+      <div className="home-content">
+        <h1>Happy Valentine's Day</h1>
+        <h2>- Abid Chowdhury</h2>
+
+        <button
+          className="valentine-btn"
+          onClick={() => navigate("/pictures")}
+        >
+          Pictures
+        </button>
+      </div>
     </div>
   );
 }
