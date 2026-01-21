010c9d61b574a6314760e1785a4615f26f41a708
Abid-Chowdhury <mr.abid2004@gmail.com>
2026-01-21 00:30:52 -0500

    Added elements to Question Page

--- src/pages/Question.js
+++ src/pages/Question.js
@@ -1,7 +1,25 @@
 export default function Question() {
   return (
-    <div className="page">
-      <h1>Question</h1>
+    <div className="question-page">
+      
+    <a href="/Pictures" className="stats-backward-link"> &lt; Pictures</a>
+      
+
+    <div className="heart">❤️</div>
+
+    <h1 className="question-title">The big question</h1>
+    <div className="question-text">Will you be my</div>
+    <div className="question-highlight">Valentine?</div>
+    <div className="question-buttons">
+      <button className="valentine-btn yes-btn">Yes 💕</button>
+      <button className="shy-btn">Just say yes!</button>
     </div>
+
+    <div className="shy-text">
+      The no button is a bit shy... just like my love for you isnt
+    </div>
+
+  </div>
+
   );
 }
